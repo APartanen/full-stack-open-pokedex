12b08544a5e255a1e0ff1ff1c6e5e7d91a4497e9
Antti Partanen <a.partanen30@gmail.com>
2025-03-17 07:23:36 +0200

    added test for pokemon page

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,5 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:5000',
-    // env: {
-    //   BACKEND: 'http://localhost:5000',
-    // },
   },
 })
--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,5 +4,10 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be opened', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
 //  cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
