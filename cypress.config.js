7e26c535e1eb973a183bacdd62b24c47855c7d95
Antti Partanen <a.partanen30@gmail.com>
2025-03-17 07:19:37 +0200

    changed baseurl port

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:8080',
-    env: {
-      BACKEND: 'http://localhost:5000',
-    },
+    baseUrl: 'http://localhost:5000',
+    // env: {
+    //   BACKEND: 'http://localhost:5000',
+    // },
   },
 })
