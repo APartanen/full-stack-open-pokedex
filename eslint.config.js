d7dada8b6dc560eddecd02623a90c62c3af4a63c
Antti Partanen <a.partanen30@gmail.com>
2025-03-17 06:42:29 +0200

    updating lint version

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,48 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    'jest/globals': true,
-    node: true,
-    'cypress/globals': true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+const { FlatCompat } = require('@eslint/eslintrc')
+const compat = new FlatCompat()
+
+module.exports = [
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+      globals: {
+        browser: true,
+        es6: true,
+        jest: true,
+        node: true,
+        cypress: true,
+      },
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'jest', 'cypress'],
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 'error',
-    'react/prop-types': 0,
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    plugins: {
+      react: require('eslint-plugin-react'),
+      jest: require('eslint-plugin-jest'),
+      cypress: require('eslint-plugin-cypress'),
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'error',
+      'react/prop-types': 0,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-  ignorePatterns: ['webpack.config.js', 'node_modules/*', 'dist/*'],
-}
+  {
+    ignores: ['webpack.config.js', 'node_modules/*', 'dist/*'],
+  },
+  ...compat.extends('eslint:recommended', 'plugin:react/recommended'),
+]
