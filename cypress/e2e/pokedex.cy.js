ba753548e93068453c547a38161cb7919da83ca2
Antti Partanen <a.partanen30@gmail.com>
2025-03-17 07:26:45 +0200

    added test for pokemon page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,10 +4,13 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+})
+//  cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
 
+describe('Pokemon page can be opened', function () {
   it('pokemon page can be opened', function () {
+    cy.visit('')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
 })
-//  cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
